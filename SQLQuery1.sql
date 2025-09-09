592ef404fb585e56d6180b7de5808ebbf0837a25
Vuong118 <quangvuong118@gmail.com>
2025-09-09 20:50:01 +0700

    8 bai tap ngay 9/9/2025

--- SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-SELECT TOP (1000) [DepartmentID]
-      ,[Name]
-      ,[GroupName]
-      ,[ModifiedDate]
-  FROM [AdventureWorks2022].[HumanResources].[Department]
-SELECT Name AS n , CONCAT_WS(', ',DepartmentID  ,GroupName ,ModifiedDate) AS ad 
-From [AdventureWorks2022].[HumanResources].[Department]
