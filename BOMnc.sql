46763e8b571f718cc09df66d2cf78ee34023309f
Vuong118 <quangvuong118@gmail.com>
2025-09-15 13:46:51 +0700

    dùng BOM tìm ra số linh kiện cần cho 10sp

--- BOMnc.sql
+++ BOMnc.sql
@@ -9,44 +9,37 @@
       ,[ModifiedDate]
   FROM [AdventureWorks2022].[Production].[BillOfMaterials]
 
-
+  --đặt sp gốc rootproduct = 811
  DECLARE @RootProductID INT = 811;
+ --tạo CTE đệ quy cha 
 WITH cha AS (
+	--chọn các linh kiện và đặt chúng làm Lv1 
 	SELECT 
-		ProductAssemblyID,
-		ComponentID,
-		CAST(PerAssemblyQTY AS DECIMAL (5,2))AS TotalQty,
+		ProductAssemblyID, --linh kiện gốc
+		ComponentID,		--linh kiện con
+		CAST(PerAssemblyQTY AS DECIMAL (5,2))AS TotalQty,-- số lượng cần cho 1 sp gốc
 		1 AS Lv
 	FROM [AdventureWorks2022].[Production].[BillOfMaterials]
+	--lọc các linh kiện là linh kiện gốc và vẫn còn bán
 	WHERE ProductAssemblyID = @RootProductID
 	AND EndDate IS NULL
 
 UNION ALL 
+--đệ quy các linh kiện con rồi tính tổng các linh kiện đặt làm lv n+1
 	SELECT 
 		g.ProductAssemblyID,
-		g.ComponentID,
+		g.ComponentID,			
 		CAST(c.TotalQty * g.PerAssemblyQty AS DECIMAL(5,2)) AS TotalQty,
 		c.Lv + 1 
 	FROM [AdventureWorks2022].[Production].[BillOfMaterials] AS g
-	INNER JOIN cha AS c 
+	INNER JOIN cha AS c  --nối bảng dữ liệu gốc với CTE cha
 	ON g.ProductAssemblyID = c.ComponentID
-	WHERE EndDate IS NULL
+	WHERE EndDate IS NULL -- lọc các linh kiện vẫn còn bán
 )
+--từ CTE cha in ra  ComponentID và tổng linh kiện con cho 10 sp 
 SELECT 
 	ComponentID,
-	SUM(TotalQty) * 10 AS TotalQtyFOR10
+	SUM(TotalQty) * 10 AS TotalQtyFOR10  -- tổng linh kiện cho 10sp bằng tổng linh kiện con *10
 FROM cha 
 GROUP BY ComponentID
 ORDER BY ComponentID ASC;
-
-/* 
-đặt rootproduct = 811
-tạo CTE đệ quy chọn các linh kiện và đặt chúng làm lv1 
-lọc các linh kiện là linh kiện gốc và vẫn còn hạn 
-UNION tất cả 
-chọn các linh kiện con rồi tính tổng các linh kiện đặt làm lv n+1
-nối bảng dữ liệu gốc với CTE cha
-dùng g.ProductAssemblyID = c.ComponentID nối bảng cha với con 
-phần Final
-từ CTE cha in ra  ComponentID và tổng linh kiện con cho 10 sp 
-*/
