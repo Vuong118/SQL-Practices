a614decfd175d5a9cd7501be8dd9461d75681d1f
Vuong118 <quangvuong118@gmail.com>
2025-09-15 22:08:55 +0700

    tìm đơn hàng đầu tiên của khách hàng

---
+++ timdonhangdautiencuakhachhang.sql
@@ -0,0 +1,15 @@
+﻿/*
+bài toán : với mỗi khách hàng tìm đơn hàng đầu tiên họ đặt
+*/
+SELECT
+    c.CustomerID,
+    d.OrderDate,
+    d.SalesOrderID
+FROM [AdventureWorks2022].[Sales].[Customer] c
+CROSS APPLY (
+    SELECT TOP 1 s.SalesOrderID, s.OrderDate
+    FROM [AdventureWorks2022].[Sales].[SalesOrderHeader] s
+    WHERE s.CustomerID = c.CustomerID
+    ORDER BY s.OrderDate ASC  -- đơn đầu tiên
+) AS d
+ORDER BY c.CustomerID;
