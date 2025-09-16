1df3c5428b971f4f1cedf5dec46d3b50df3c7767
Vuong118 <quangvuong118@gmail.com>
2025-09-16 13:56:11 +0700

    trung binh chi tiêu của top3 khách hàng và top1 khách hàng tiêu nhiều nhất

---
+++ topchitieunhieu.sql
@@ -0,0 +1,42 @@
+﻿/*
+WITH khachhang AS (
+    SELECT 
+        h.CustomerID,
+        SUM(d.OrderQty * d.UnitPrice) AS Tongdoanhthu
+    FROM [AdventureWorks2022].[Sales].[SalesOrderHeader] h
+    JOIN [AdventureWorks2022].[Sales].[SalesOrderDetail] d
+        ON h.SalesOrderID = d.SalesOrderID
+    GROUP BY h.CustomerID
+)
+ bài toán dùng bảng view trên : Tìm trung bình doanh thu của Top 3 khách hàng chi tiêu nhiều nhất ở mỗi TerritoryID và xác định Territory nào có giá trị này cao nhất.
+
+
+
+*/
+-- Lấy tổng chi tiêu của từng khách và gắn Territory
+WITH ranker AS (
+    SELECT 
+        c.TerritoryID,
+        k.CustomerID,
+        k.Tongdoanhthu AS Tongchitieu,
+        ROW_NUMBER() OVER(PARTITION BY c.TerritoryID ORDER BY k.Tongdoanhthu DESC) AS bxh
+    FROM khachhang AS k
+    JOIN [AdventureWorks2022].[Sales].[Customer] AS c
+        ON k.CustomerID = c.CustomerID
+),
+
+--Tính toán trung bình Top 3 và khách chi nhiều nhất
+tbchitieu AS (
+    SELECT 
+		
+        TerritoryID,
+        MAX(CASE WHEN bxh IS NOT NULL THEN Tongchitieu END) AS tieunhieunhat,
+        AVG(Tongchitieu) AS trungbinhdoanhthu,
+		bxh
+    FROM ranker
+			WHERE bxh <=3
+    GROUP BY TerritoryID,bxh
+)
+SELECT *
+FROM tbchitieu
+ORDER BY TerritoryID;
