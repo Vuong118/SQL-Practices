ca73014da3787288d2872ae7a01f494f034abb9e
Vuong118 <quangvuong118@gmail.com>
2025-09-18 17:05:09 +0700

    đơn hàng giá cao bất thường

---
+++ donhangbatthuong.sql
@@ -0,0 +1,27 @@
+﻿/*
+ĐỀ Bài – Đơn hàng bất thường
+
+Trong bảng Sales.SalesOrderDetail:
+
+Hãy tìm những đơn hàng có giá trị lớn hơn 2 lần giá trị trung bình của tất cả đơn hàng.
+
+Kết quả: SalesOrderID, TongGiaTri, GiaTriTrungBinh, HeSo.
+*/
+WITH giatri AS (
+    SELECT
+        S.SalesOrderID,
+        SUM(S.UnitPrice) AS Tonggiatri
+    FROM [AdventureWorks2022].[Sales].[SalesOrderDetail] S
+    GROUP BY S.SalesOrderID
+),
+tb AS (
+    SELECT AVG(Tonggiatri * 1.0) AS GiatriTrungBinh
+    FROM giatri
+)
+SELECT 
+    g.SalesOrderID,
+    g.Tonggiatri,
+    CAST(g.Tonggiatri * 1.0 / tb.GiatriTrungBinh AS DECIMAL(10,2)) AS HeSo
+FROM giatri g
+CROSS JOIN tb
+WHERE g.Tonggiatri > 2 * tb.GiatriTrungBinh;
